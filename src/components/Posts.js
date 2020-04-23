6b165082e6757bab15e1a289840ecd951ee4ffff
Sophia <sophierithous@gmail.com>
2020-04-23 16:09:31 +0100

    a bit of styling

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Post extends Component {
   render() {
     return (
-      <div>
+      <div className="card">
         <h2>{this.props.post.title}</h2>
         <p>{this.props.post.message}</p>
       </div>
